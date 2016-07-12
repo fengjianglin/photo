a3afd056221ab2a1b23edbb53f82d48e01056995
feng <fandyvon@163.com>
2016-07-12 15:22:44 +0800

    compress

--- src/main/java/photo/baby/service/impl/PhotoServiceImpl.java
+++ src/main/java/photo/baby/service/impl/PhotoServiceImpl.java
@@ -1,5 +1,8 @@
 package photo.baby.service.impl;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifIFD0Directory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
@@ -13,13 +16,20 @@ import photo.baby.repository.PhotoRepository;
 import photo.baby.repository.PromptRepository;
 import photo.baby.service.AlbumService;
 import photo.baby.service.PhotoService;
+import sun.awt.image.ImageAccessException;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.FileImageInputStream;
+import javax.imageio.stream.IIOByteBuffer;
+import javax.imageio.stream.ImageOutputStream;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -47,16 +57,67 @@ public class PhotoServiceImpl implements PhotoService, AlbumService {
 
     @Override
     public Photo save(MultipartFile multipartFile, String name) throws IOException {
+
+
+        File tempImage = new File(fileDir, "tmp_" + name);
         File image = new File(fileDir, name);
-        multipartFile.transferTo(image);
+
+
+        InputStream inputStream = multipartFile.getInputStream();
+
+        OutputStream outputStream = new FileOutputStream(image);
+
+        compressImage(inputStream, outputStream);
+
         BufferedImage bufferedImage = ImageIO.read(image);
+
+
+        int orientation = 1;
+        try {
+            Metadata metadata = ImageMetadataReader.readMetadata(image);
+            ExifIFD0Directory exifIFD0Directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+            orientation = exifIFD0Directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         Photo photo = new Photo();
         photo.setName(name);
-        photo.setWidth(bufferedImage.getWidth());
-        photo.setHeight(bufferedImage.getHeight());
+        if (orientation > 4) {
+            photo.setWidth(bufferedImage.getHeight());
+            photo.setHeight(bufferedImage.getWidth());
+        } else {
+            photo.setWidth(bufferedImage.getWidth());
+            photo.setHeight(bufferedImage.getHeight());
+        }
+
         photo.setSize(multipartFile.getSize());
         photo.setCreatedAt(new Date());
         return photoRepository.save(photo);
+    }
+
+    private void compressImage(InputStream inputStream, OutputStream outputStream) throws IOException {
+        float imageQuality = 0.3f;
+        BufferedImage bufferedImage = ImageIO.read(inputStream);
+        Iterator<ImageWriter> imageWriters = ImageIO.getImageWritersByFormatName("jpg");
+        if (!imageWriters.hasNext()) {
+            throw new IllegalStateException("Writers Not Found!!");
+        }
+        ImageWriter imageWriter = imageWriters.next();
+        ImageOutputStream imageOutputStream = ImageIO.createImageOutputStream(outputStream);
+        imageWriter.setOutput(imageOutputStream);
+
+        ImageWriteParam imageWriteParam = imageWriter.getDefaultWriteParam();
+
+        imageWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        imageWriteParam.setCompressionQuality(imageQuality);
+
+        //Created image
+        imageWriter.write(null, new IIOImage(bufferedImage, null, null), imageWriteParam);
+        inputStream.close();
+        outputStream.close();
+        imageOutputStream.close();
+        imageWriter.dispose();
     }
 
     public boolean delete(Photo photo) {
